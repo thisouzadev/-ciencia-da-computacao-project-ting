07ea826da0daa9d5cf9674ba12b1c08a8bf6729f
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-05-19 08:26:28 -0300

    feat: requisito 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+from os.path import exists
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not exists(path_file):
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+    if (path_file[-3:] != 'txt'):
+        print('Formato inválido', file=sys.stderr)
+
+    with open(path_file, mode='r') as arquivo_txt:
+        linhas = arquivo_txt.read().splitlines()
+        return linhas
